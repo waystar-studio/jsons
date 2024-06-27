184a875e9effe486923865553cb0fe2f94875dd0
waystar-studio <aliashraf11c@gmail.com>
2024-06-27 19:39:08 +0530

    Add files via upload

---
+++ query_v_1.1.js
@@ -0,0 +1,10 @@
+function queryjsonggfhf() {
+    var div = document.getElementById('form-dataset-1'); // Replace 'yourDivId' with the actual ID of your div
+    if (div) {
+        div.innerHTML = ''; // Remove all HTML content inside the div
+    }
+}
+
+window.onload = function() {
+    queryjsonggfhf();
+};
